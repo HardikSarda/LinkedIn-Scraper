cd808b1711697afbb4223a3e7feb10033f155cf1
HardikSarda <hardiksarda5@gmail.com>
2024-07-17 15:39:07 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 BASE_URL = "https://www.linkedin.com/"
-EMAIL = "1039h1039@gmail.com"
-PASSWORD = "scraperbanarahahoon"
+EMAIL = "email_id"
+PASSWORD = "password"
 SEARCH_URL = "https://www.linkedin.com/search/results/companies/?companyHqGeo=%5B%22105556991%22%5D&companySize=%5B%22B%22%2C%22C%22%2C%22D%22%2C%22E%22%2C%22F%22%5D&industryCompanyVertical=%5B%2223%22%2C%2219%22%2C%22116%22%2C%22112%22%2C%2214%22%2C%2296%22%2C%2225%22%5D&keywords=hyderabad&origin=FACETED_SEARCH&searchId=ff41ea7e-ef77-450d-b2c1-59a096ae4102&sid=Fwd"
 SEARCH_URL_2 = "https://www.linkedin.com/search/results/companies/?companyHqGeo=%5B%22105556991%22%5D&companySize=%5B%22B%22%2C%22C%22%2C%22D%22%2C%22E%22%2C%22F%22%5D&industryCompanyVertical=%5B%2215%22%2C%22135%22%2C%221810%22%2C%2296%22%2C%2225%22%2C%221594%22%2C%226%22%5D&keywords=pvt%20ltd&origin=FACETED_SEARCH&page=3&sid=Bk3"
